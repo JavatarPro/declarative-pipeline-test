54b8ca69126930ab22de7fa2e08ea2c85ec55ea4
Borys Zora <borys.zora@javatar.pro>
2019-11-10 22:06:43 +0200

    CD-39: draft refactoring test configuration and decouple from jenkins dsl in domain classes

--- src/test/groovy/pro/javatar/pipeline/mock/JenkinsDslServiceMock.groovy
+++ src/test/groovy/pro/javatar/pipeline/mock/JenkinsDslServiceMock.groovy
@@ -2,20 +2,30 @@
  * Copyright (c) 2019 Javatar LLC
  * All rights reserved.
  */
-package pro.javatar.pipeline.mock;
+package pro.javatar.pipeline.mock
 
-import pro.javatar.pipeline.service.JenkinsDslService
+import groovy.util.logging.Slf4j;
+import pro.javatar.pipeline.jenkins.api.JenkinsDslService
+import pro.javatar.pipeline.jenkins.api.JenkinsExecutor
 import pro.javatar.pipeline.stage.StageAware
+
+import java.time.Duration
 
 /**
  * @author Borys Zora
  * @version 2019-11-03
  */
+@Slf4j
 class JenkinsDslServiceMock implements JenkinsDslService {
 
     @Override
     void executeStage(StageAware stage) {
         stage.execute();
+    }
+
+    @Override
+    void executeWithinTimeoutInSpecifiedDirectory(Duration timeout, String directory, JenkinsExecutor executor) {
+        log.info("timeout: {}, directory: {}", timeout, directory);
     }
 
 }
