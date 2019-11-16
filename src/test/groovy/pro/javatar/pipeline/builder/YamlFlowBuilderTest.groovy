be00793a9799109f4e5cf1a378476a9a36dc2102
Borys Zora <borys.zora@javatar.pro>
2019-11-17 01:08:37 +0200

    CD-39: add unit tests for auto tests configuration

--- src/test/groovy/pro/javatar/pipeline/builder/YamlFlowBuilderTest.groovy
+++ src/test/groovy/pro/javatar/pipeline/builder/YamlFlowBuilderTest.groovy
@@ -6,12 +6,17 @@ package pro.javatar.pipeline.builder
 
 import groovy.util.logging.Slf4j
 import pro.javatar.pipeline.Flow
+import pro.javatar.pipeline.config.AutoTestConfig
+import pro.javatar.pipeline.config.Config
+import pro.javatar.pipeline.config.GradleConfig
 import pro.javatar.pipeline.jenkins.api.JenkinsDslService
 import pro.javatar.pipeline.mock.JenkinsDslServiceMock
 import pro.javatar.pipeline.mock.PipelineDslHolderMock
 import pro.javatar.pipeline.service.PipelineDslHolder
 import spock.lang.Shared
 import spock.lang.Specification
+
+import java.time.Duration
 
 /**
  * @author Borys Zora
@@ -31,22 +36,32 @@ class YamlFlowBuilderTest extends Specification {
         dslService = new JenkinsDslServiceMock();
     }
 
-    def "test build"() {
+    def "flow configuration and execution for gradle with nomad"() {
         YamlFlowBuilder builder = new YamlFlowBuilder(GRADLE_NOMAD_CONFIG_FILE, dslService);
         given: "gradle configuration in " + GRADLE_NOMAD_CONFIG_FILE
 
         when: "build flow is completed"
+        Config config = builder.getEffectiveConfig(GRADLE_NOMAD_CONFIG_FILE);
         Flow flow = builder.build();
 
-        then: "expected configuration"
+        then: "expected configuration is correct"
+        GradleConfig gradleConfig = config.gradleConfig();
 
-        expect:
-        1==1;
+        expect: "gradle config is correct"
+        gradleConfig.gradleTool() == "gradle_5.6.3"
+        gradleConfig.javaTool() == "JDK11"
+        gradleConfig.additionalBuildParameters() == ""
+        gradleConfig.versionFile() == "gradle.properties"
+        gradleConfig.repositoryUrl() == "http://127.0.0.1:8080/repository/maven-releases/"
+        gradleConfig.repositoryId() == "nexus"
 
-        when: "execute flow"
+        and: "auto test config is correct"
+        AutoTestConfig autoTestConfig = config.autoTestConfig();
 
-        then:
-        1==1;
+        autoTestConfig.enabled();
+        autoTestConfig.timeout() == Duration.parse("PT7M");
+        autoTestConfig.initialDelay() == Duration.parse("PT30S")
+        autoTestConfig.jobName() == "system-tests"
     }
 
 }
