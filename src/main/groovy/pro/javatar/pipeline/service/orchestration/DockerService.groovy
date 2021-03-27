af5add5c50eff2de5d8d3e5de853ccb7172ac530
borys.zora <borys.zora@javatar.pro>
2021-03-28 01:36:12 +0200

    fix folder stuff for docker build 3

--- src/main/groovy/pro/javatar/pipeline/service/orchestration/DockerService.groovy
+++ src/main/groovy/pro/javatar/pipeline/service/orchestration/DockerService.groovy
@@ -89,13 +89,14 @@ class DockerService implements Serializable {
         dsl.sh "docker build -t ${imageName}:${imageVersion} ${getCustomDockerFileInstruction()} ."
         Logger.debug("dockerBuildImage for service: ${imageName} with releaseVersion: ${imageVersion} finished")
     }
-    
+
     def dockerBuildImageWithContextOptimizationForUI(ReleaseInfo releaseInfo) {
         Logger.debug("dockerBuildImageWithContextOptimizationForUI started")
-        dsl.sh "cd ${releaseInfo.getBuildDockerFromFolder()}"
-        dsl.sh "mv ../${releaseInfo.getUiDistributionFolder()} ."
-        dockerBuildImage(releaseInfo.getDockerImageName(), releaseInfo.getDockerImageVersion())
-        dsl.sh "cd .."
+        dsl.sh "mv ${releaseInfo.getUiDistributionFolder()} ${releaseInfo.getBuildDockerFromFolder()}"
+        dsl.dir (releaseInfo.getBuildDockerFromFolder()) {
+            dsl.sh "pwd && ls -la && ls -la .."
+            dockerBuildImage(releaseInfo.getDockerImageName(), releaseInfo.getDockerImageVersion())
+        }
         Logger.debug("dockerBuildImageWithContextOptimizationForUI finished")
     }
 
